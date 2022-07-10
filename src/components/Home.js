53e677741626db9474bd0bcd374a017feea99c4f
unknown <engraaadeyemi@gmail.com>
2022-07-10 17:13:40 +0100

    fixed responsiveness

--- src/components/Home.js
+++ src/components/Home.js
@@ -19,8 +19,8 @@ const Home = () => {
   };
 
   const divStyle = "flex items-center flex-col";
-  const pTag1 = "font-bold text-2xl text-white";
-  const pTag2 = "font-[400] text-xl text-white";
+  const pTag1 = "font-bold lg:text-4xl text-white text-2xl";
+  const pTag2 = "font-[400] text-[1rem] md:text-xl lg:text-2xl text-white";
 
   return (
     <section className="wrapper h-[100vh] -z-10 ">
@@ -29,16 +29,16 @@ const Home = () => {
         onKeyPress={searchLocation}
         onChange={(e) => setLocation(e.target.value)}
         type="text"
-        placeholder="ENTER YOUR LOCATION (Country, State, City, etc..)"
-        className="mx-auto top-[4rem] absolute left-0 right-0 w-1/2 lg:w-[30%] glass border border-gray-500 h-12 outline-none pl-6 text-white placeholder:text-gray-300 placeholder:font-semibold font-semibold "
+        placeholder="ENTER YOUR LOCATION"
+        className="mx-auto top-[4rem] absolute left-0 right-0 w-[60%] md:w-[50%] lg:w-[40%] glass border border-gray-500 h-12 outline-none pl-4 text-white placeholder:text-gray-300 placeholder:font-semibold font-semibold text-[.5rem] lg:text-[.8rem] lg:text-xl uppercase"
       />
       <div className="flex justify-between pl-5 w-full mt-[13rem] ">
         <div className="ml-0 xl:ml-20">
-          <p className="text-2xl lg:text-3xl text-white font-bold uppercase">
+          <p className="text-xl lg:text-3xl  text-white font-bold uppercase">
             {data.name}
           </p>
           {data.name ? (
-            <p className="lg:text-9xl text-8xl font-bold text-white">
+            <p className="lg:text-9xl md:text-8xl text-7xl font-bold text-white">
               {Math.round(data.main.temp)}
               <sup>o</sup>F
             </p>
@@ -54,7 +54,7 @@ const Home = () => {
           <p className="-rotate-90 text-2xl lg:text-3xl text-white font-bold xl:mr-10 -mr-10"></p>
         )}
       </div>
-      <div className="w-[90%] lg:w-[80%] xl:w-[70%] h-32 absolute bottom-6 mx-auto left-0 right-0 glass flex justify-around items-center">
+      <div className="w-[90%] lg:w-[80%] xl:w-[70%] h-28 absolute bottom-6 mx-auto left-0 right-0 glass flex justify-around items-center">
         {data.name ? (
           <div className={divStyle}>
             <p className={pTag1}>
@@ -82,7 +82,7 @@ const Home = () => {
         )}
         {data.name ? (
           <div className={divStyle}>
-            <p className={pTag1}>{data.wind.speed}MPH</p>
+            <p className={pTag1}>{data.wind.speed}mph</p>
             <p className={pTag2}>Wind Speed</p>
           </div>
         ) : (
