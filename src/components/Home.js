0221ffe3accded73e644d4caf7b81391f6725436
unknown <engraaadeyemi@gmail.com>
2022-07-10 16:45:57 +0100

    done 100%

---
+++ src/components/Home.js
@@ -0,0 +1,98 @@
+import React, { useState } from "react";
+
+const Home = () => {
+  const [location, setLocation] = useState("");
+  const [data, setData] = useState({});
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=imperial&appid=56898df3e21e6f12403d03cd4d3eba67`;
+
+  const searchLocation = (event) => {
+    if (event.key === "Enter") {
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((data) => {
+          setData(data);
+        })
+        .catch((error) => error);
+      setLocation("");
+    }
+  };
+
+  const divStyle = "flex items-center flex-col";
+  const pTag1 = "font-bold text-2xl text-white";
+  const pTag2 = "font-[400] text-xl text-white";
+
+  return (
+    <section className="wrapper h-[100vh] -z-10 ">
+      <input
+        value={location}
+        onKeyPress={searchLocation}
+        onChange={(e) => setLocation(e.target.value)}
+        type="text"
+        placeholder="ENTER YOUR LOCATION (Country, State, City, etc..)"
+        className="mx-auto top-[4rem] absolute left-0 right-0 w-1/2 lg:w-[30%] glass border border-gray-500 h-12 outline-none pl-6 text-white placeholder:text-gray-300 placeholder:font-semibold font-semibold "
+      />
+      <div className="flex justify-between pl-5 w-full mt-[13rem] ">
+        <div className="ml-0 xl:ml-20">
+          <p className="text-2xl lg:text-3xl text-white font-bold uppercase">
+            {data.name}
+          </p>
+          {data.name ? (
+            <p className="lg:text-9xl text-8xl font-bold text-white">
+              {Math.round(data.main.temp)}
+              <sup>o</sup>F
+            </p>
+          ) : (
+            <p className="lg:text-3xl text-2xl font-bold text-white pl-16"></p>
+          )}
+        </div>
+        {data.name ? (
+          <p className="-rotate-90 text-2xl lg:text-3xl text-white font-bold xl:mr-10 -mr-10">
+            {data.weather[0].main}
+          </p>
+        ) : (
+          <p className="-rotate-90 text-2xl lg:text-3xl text-white font-bold xl:mr-10 -mr-10"></p>
+        )}
+      </div>
+      <div className="w-[90%] lg:w-[80%] xl:w-[70%] h-32 absolute bottom-6 mx-auto left-0 right-0 glass flex justify-around items-center">
+        {data.name ? (
+          <div className={divStyle}>
+            <p className={pTag1}>
+              {Math.round(data.main.feels_like)}
+              <sup>o</sup>F
+            </p>
+            <p className={pTag2}>Feels Like</p>
+          </div>
+        ) : (
+          <div className={divStyle}>
+            <p className={pTag1}></p>
+            <p className={pTag2}>Feels Like</p>
+          </div>
+        )}
+
+        {data.name ? (
+          <div className={divStyle}>
+            <p className={pTag1}>{data.main.humidity}%</p>
+            <p className={pTag2}>Humidity</p>
+          </div>
+        ) : (
+          <div className={divStyle}>
+            <p className={pTag2}>Humidity</p>
+          </div>
+        )}
+        {data.name ? (
+          <div className={divStyle}>
+            <p className={pTag1}>{data.wind.speed}MPH</p>
+            <p className={pTag2}>Wind Speed</p>
+          </div>
+        ) : (
+          <div className={divStyle}>
+            <p className={pTag2}>Wind Speed</p>
+          </div>
+        )}
+      </div>
+    </section>
+  );
+};
+
+export default Home;
